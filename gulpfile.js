06de19f4623eb5966a8d09f64ac5bb801dc5d034
Sheplu <jean.burellier@gmail.com>
2016-02-02 20:54:31 +0100

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    del = require('del');
+
+gulp.task('jshint', function() {
+  return gulp.src('app/scripts/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('usemin', 'imagemin','copyfonts');
+});
