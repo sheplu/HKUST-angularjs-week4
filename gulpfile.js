4d8da4b6c975f15592c630e158bdbcd0cc7505e9
Sheplu <jean.burellier@gmail.com>
2016-02-03 21:02:27 +0100

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -31,10 +31,10 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/**/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
-        js: [uglify(),rev()]
+        js: [ngannotate(),uglify(),rev()]
       }))
       .pipe(gulp.dest('dist/'));
 });
@@ -73,11 +73,11 @@ gulp.task('browser-sync', ['default'], function () {
 ];
 
 browserSync.init(files, {
-  server: {
-     baseDir: "dist",
-     index: "menu.html"
-  }
-});
+      server: {
+         baseDir: "dist",
+         index: "index.html"
+      }
+   });
     // Watch any files in dist/, reload on change
 gulp.watch(['dist/**']).on('change', browserSync.reload);
 });
